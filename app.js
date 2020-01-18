8d656816f3b9467e01b100ad9e01bb76a70f969d
Sammii-HK <sammii.h@icloud.com>
2020-01-18 16:39:55 +0000

    set up basic scene

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-const router = express.Router()
 const port = 4000
+const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public')))
-//
-// app.get('/', (req, res) => res.send('Hello World!'))
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
 
-app.use('/', router)
+app.get('/', (req, res) => res.json('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/main.js
+++ public/main.js
@@ -1,3 +1,34 @@
+console.log('JS loaded')
+
+// IMPORT PACKAGES
+var THREE = require('three')
+import { FBXLoader, GLTFLoader, OrbitControls, WebGL } from 'THREE/examples/jsm/controls/';
+// GLOBAL VARIABLES
+var controls, camera, scene, render, light, mixer;
+// CLOCK
+var clock = new THREE.Clock();
+// CREATE A SCENE
+var scene = new THREE.Scene()
+scene.background = new THREE.Color(0x6a99ee)
+scene.fog = new THREE.Fog(0x6a99ee, 500, 1000)
+// CREATE CAMERA
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 100, 250)
+scene.add(camera)
+// CREATE RENDERER
+var renderer = new THREE.WebGLRenderer();
+rander.setPixelRatio(window.devicePixelRatio)
+renderer.setSize( window.innerWidth, window.innerHeight );
+render.shadowMap.enabledd = true
+render.gammaOutput = true
+render.gammaFactor = 2.2
+
+
+
+document.body.appendChild( renderer.domElement );
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JS loaded')
+  console.log("DOMContentLoaded")
 })
