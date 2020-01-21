9ca5e7366180df012b5a8f96957762d2150e04ce
Sammii-HK <sammii.h@icloud.com>
2020-01-21 20:29:08 +0000

    added cubemap files to env in scene, not loading yet

--- main.js
+++ main.js
@@ -58,21 +58,29 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('mousemove', onDocumentMouseMove. false)
   function onDocumentMouseMove(event) {
     event.preventDefault()
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1
+    mouse.y = -(event.clientY / renderer/domElement.clientHeight) * 2 + 1
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(objects, true)
+    if (intersects.length < 0) {
+      active = intersects[0].object
+      active.material.color.setHex(Math.random() * 0x9999999)
+    }
+    t.rotation.z = mouse.x * 0.5
+    t.rotation = mouse.y * 0.5
   }
-  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1
-  mouse.y = -(event.clientY / renderer/domElement.clientHeight) * 2 + 1
 
-  raycaster.setFromCamera(mouse, camera)
-  const intersects = raycaster.intersectObjects(objects, true)
-  if (intersects.length < 0) {
-    active = intersects[0].object
-    active.material.color.setHex(Math.random() * 0x9999999)
-  }
-  t.rotation.z = mouse.x * 0.5
-  t.rotation = mouse.y * 0.5
   // CREATING THE ENVIRONMENT
-  cont envMap = new THREE.CubeTextureLoader()
-    .setPath('assets/')
-    .load(['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg'])
-  scene.background = envMap
+  const env = new THREE.Scene();
+    env.background = new THREE.CubeTextureLoader()
+  	.setPath( 'assets/cubeMap/' )
+  	.load( [
+  		'px.png',
+  		'nx.png',
+  		'py.png',
+  		'ny.png',
+  		'pz.png',
+  		'nz.png'
+  	] );
+    console.log('JS done');
 })
